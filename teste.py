e9809f0cac8464e3a5d2a61d5df7645980bd38d9
Vinicius Brito <81504174+vinicbri@users.noreply.github.com>
2024-01-10 15:32:40 -0300

    Update teste.py
    Tinha erros que eu achei então melhorei o codigo, basicamente conseguimos pre processar qualquer data.csv que colocar nele, espero que gostem !!

--- teste.py
+++ teste.py
@@ -20,23 +20,34 @@ y = dataset['Nome do dataset target']
 print(X)
 print (y)
 
+# identificar as colunas numericas e categoricas
+categorical_cols = [col for col in X.columns if X[col].dtype == 'object']
+numeric_cols = [col for col in X.columns if col not in categorical_cols]
+
 # Estou trocando o valor de valores nas colunas desconhecidos pela média de valor daquela coluna
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X)
-X = imputer.transform(X)
+X[numeric_cols] = imputer.fit_transform(X[numeric_cols])
 
+# Crie uma instância da classe StandardScaler e one hot encoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_cols)], remainder='passthrough')
+X = ct.fit_transform(X)
+
+# Codifica o target (se for categórico)
+if y.dtype == 'object':
+    le = LabelEncoder()
+    y = le.fit_transform(y)
+  
 #Divida o conjunto de dados em um conjunto de treinamento-teste de 80-20
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Crie uma instância da classe StandardScaler
-sc = StandardScaler()
-
 # Aplique o StandardScaler nas características do conjunto de treinamento e faça a transformação 
-X_train_scaled = sc.fit_transform(X_train)
-X_test_scaled = sc.transform(X_test)
+sc = StandardScaler()
+num_columns = len(categorical_cols)
+X_train[:, num_columns:] = sc.fit_transform(X_train[:, num_columns:])
+X_test[:, num_columns:] = sc.transform(X_test[:, num_columns:])
 
-# Imprima os conjuntos de dados de treinamento e teste escalados
-print("Scaled training set:\n", X_train_scaled) 
-print("Scaled test set:\n", X_test_scaled)
+# Imprima os conjuntos de dados de treinamento e teste escalados se quiser
+print("Scaled training set:\n", X_train) 
+print("Scaled test set:\n", X_test)
 
 
